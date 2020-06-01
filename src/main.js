7d0930db3a9c4db651fcc88844ad14033e23a34e
estefania moyano <estefaniamoyano@Airdeestefania.home>
2020-05-31 21:25:36 -0400

    css H.U 1

--- src/main.js
+++ src/main.js
@@ -5,26 +5,27 @@
 myFunction(); */
 
 //Enlace de elementos al HTML
-const viewAuthUser = document.getElementById("contenedor");
+const viewAuthUser = document.getElementById("container");
 
 //Funcion que crea los elementos de inicio de sesion
 const userLoginView = () => {
   let userLogin = "";
   userLogin += `<!-- Inicio -->
+  <div class="bodyBox">
         <div class="headerPrincipal">
           <header class="headerContent">
-            <a id="logoPrincipal" class="logoPrincipal" href="#"><img src="img/logoPrincipal.png" alt=""></a>
+            <a class="logoPrincipal" href="#"><img id="logoPrincipal" src="img/logo2-01.png" alt="Logo app"></a>
           </header>
         </div>
         <!--  -->
         <div class="bodyUser">
           <div class="userlogin">
-            <p class="txtUser">Usuario</p>
-            <input type="email" id="email" class="email" placeholder="Ingrese E-mail">
-            <p class="txtPassword">Contraseña</p>
-            <input type="password" id="password" class="password" placeholder="Ingrese Contraseña">
+            <p class="txtUser"></p>
+            <input type="email" id="email" class="email" placeholder="     Usuario">
+            <p class="txtPassword"></p>
+            <input type="password" id="password" class="password" placeholder="     Contraseña">
             </br>
-            <a href="#">¿Olvidaste tu Contraseña?</a>
+            <a href="#" id=passRecover>¿Olvidaste tu Contraseña?</a>
             </br>
             <a href="#" id="btnLogin" class="btnLogin">
               <span id="span1"></span>
@@ -39,13 +40,15 @@ const userLoginView = () => {
           <a href="#">Iniciar Sesión con Google</a>
         </div>
         <div class="bodyUserRegistrar">
-          <a href="#">¿No tienes cuenta? Registrate</a>
+          <p id= "bodyUserRegistrar" >¿No tienes una cuenta?</p> <a href="#" id="re" > Registrate </a>
         </div>
         <!--  -->
         <div class="footer">
-          <p>© 2020 Old Beat Music</p>
+          <p>© min Corp.</p>
         </div>
       </div>
+    </div>
+    
       <!-- Fin -->`;
   viewAuthUser.innerHTML = userLogin;
 };
