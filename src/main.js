7b38a624c640490dc825ee091b11364e60529d72
Niamoyano91 <62710894+Niamoyano91@users.noreply.github.com>
2020-06-02 22:17:55 -0400

    Revert "Merge Vistas, enlaces de Inicio de sesión y de registro de usuario "

--- src/main.js
+++ src/main.js
@@ -1,16 +1,6 @@
-import { routes } from "./routes/index.routes.js";
+// Este es el punto de entrada de tu aplicacion
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyCi3YgirCn8pkyynZD1ivOy0l2zHgeXcXY",
-  authDomain: "oldbeat-fdfd8.firebaseapp.com",
-  databaseURL: "https://oldbeat-fdfd8.firebaseio.com",
-  projectId: "oldbeat-fdfd8",
-  storageBucket: "oldbeat-fdfd8.appspot.com",
-  messagingSenderId: "871020668050",
-  appId: "1:871020668050:web:24ce924e5592ac3ea7f24b",
-  measurementId: "G-27R5W16948",
-
+/* import { myFunction } from "./lib/index.js";
 
 myFunction(); */
 
@@ -61,15 +51,21 @@ const userLoginView = () => {
     
       <!-- Fin -->`;
   viewAuthUser.innerHTML = userLogin;
-
 };
+userLoginView();
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+//Variables Input de Inicio de Sesion
+let btnLogin = document.getElementById("btnLogin");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
 
-// Rutas
-routes(window.location.hash);
-window.addEventListener("hashchange", () => {
-  routes(window.location.hash);
+//Evento Click del inicio de Sesion
+btnLogin.addEventListener("click", ()=> {
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  console.log(emailValue);
+  console.log(passwordValue);
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
 });
+
