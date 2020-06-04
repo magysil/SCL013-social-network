e9748a997a370d973bc0b85424d1865c33d249a4
magysil <62733263+magysil@users.noreply.github.com>
2020-06-03 23:06:49 -0400

    error 404

--- src/lib/fireBase.js
+++ src/lib/fireBase.js
@@ -1,5 +1,4 @@
 // Funcion Registrar Usuario
-
 export const authRegister = (email, password) =>
   firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
       var errorCode = error.code;
